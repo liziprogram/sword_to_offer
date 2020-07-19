5812b333c7c65f3205cf43d6b1767b4f94e4cf49
liziprogram <liziprogram@qq.com>
2020-07-19 21:53:57 +0800

    feat: 从尾到头打印链表

---
+++ src/main/java/SwordOffer06.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * 剑指 Offer 06. 从尾到头打印链表
+ * https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+ */
+
+
+public class SwordOffer06 {
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(3);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(1);
+
+        System.out.println(Arrays.toString(reversePrint(listNode)));
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static int[] reversePrint(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head);
+            head = head.next;
+        }
+        int size = stack.size();
+        int[] result = new int[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = stack.pop().val;
+        }
+        return result;
+    }
+
+}
